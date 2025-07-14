443064fc955f1f90dd75fda33e16b1428fc329c7
Valeria Gutu <munteanu.valeria98@gmail.com>
2025-07-14 13:40:14 +0300

    add new page

---
+++ DesignInterior_WebSite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesignInterior_WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+
+            return View();
+        }
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+        public ActionResult Portofolio()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
